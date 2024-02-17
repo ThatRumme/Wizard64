c206277652d36b16195f8cd78cd2da8f47fc55f0
Rune Soender <thatrumme@gmail.com>
2024-02-17 21:39:23 +0100

    Lightning ability + triggers

--- Assets/Scripts/Character/Abilities/LightningBeam.cs
+++ Assets/Scripts/Character/Abilities/LightningBeam.cs
@@ -4,9 +4,46 @@ using UnityEngine;
 
 public class LightningBeam : Ability
 {
+
+    public int maxRange = 100;
     public override void Activate()
     {
         base.Activate();
+
+        
+        //Layermask everything except these layers
+        int layerMask = 1 << LayerMask.NameToLayer("IgnoreRaycast") | 1 << LayerMask.NameToLayer("Player");
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        Physics.Raycast(transform.position, Camera.main.transform.TransformDirection(Vector3.forward), out hit, maxRange, layerMask);
+        if (hit.collider)
+        {
+            bool hitInteractableObject = false;
+            
+            if(hit.collider.CompareTag("Interactable_Electric"))
+            {
+                hitInteractableObject = true;
+                hit.collider.gameObject.GetComponent<ElectricTrigger>().Activate();
+            }
+
+            if(hitInteractableObject)
+            {
+                Vector3 dist = hit.collider.transform.position - transform.position;
+
+                Debug.DrawRay(transform.position, dist.normalized * hit.distance, Color.yellow, 2);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, Camera.main.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow, 2);
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, Camera.main.transform.TransformDirection(Vector3.forward) * 100, Color.yellow, 2);
+        }
+        
     }
     public override void Deactivate()
     {
